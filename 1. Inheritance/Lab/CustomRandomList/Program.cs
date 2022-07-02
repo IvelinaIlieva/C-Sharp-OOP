e5ec68d34f5f50d25c4e198986bdd4254f76f85a
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-07-02 20:45:39 +0300

    Initial Commit
    Add Lab

---
+++ 1. Inheritance/Lab/CustomRandomList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            RandomList list = new RandomList() {"Iva", "Plamen", "Todor"};
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
---
+++ 1. Inheritance/Lab/CustomStack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+
+            List<string> list = new List<string>() {"Iva", "Plamen", "Todor"};
+
+            stack.AddRange(list);
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
