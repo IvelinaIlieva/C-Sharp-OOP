7274f97acc7bfb873bf6fc940a708358e51d8669
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-07-03 02:56:54 +0300

    Initial Commit
    Add Exercise

---
+++ Inheritance/Exercise/P06. Animals/Animal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+        protected Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get => this.gender;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(this.GetType().Name);
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine(this.ProduceSound());
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Inheritance/Exercise/P06. Animals/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string type;
+
+            while ((type = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    string[] animalInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    string name = animalInfo[0];
+                    int age = int.Parse(animalInfo[1]);
+
+                    switch (type)
+                    {
+                        case "Dog":
+                            string genderDog = animalInfo[2];
+                            Dog dog = new Dog(name, age, genderDog);
+                            animals.Add(dog);
+                            break;
+
+                        case "Frog":
+                            string genderFrog = animalInfo[2];
+                            Frog frog = new Frog(name, age, genderFrog);
+                            animals.Add(frog);
+                            break;
+
+                        case "Cat":
+                            string genderCat = animalInfo[2];
+                            Cat cat = new Cat(name, age, genderCat);
+                            animals.Add(cat);
+                            break;
+
+                        case "Kitten":
+                            Kitten kitten = new Kitten(name, age);
+                            animals.Add(kitten);
+                            break;
+
+                        case "Tomcat":
+                            Tomcat tomcat = new Tomcat(name, age);
+                            animals.Add(tomcat);
+                            break;
+
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
