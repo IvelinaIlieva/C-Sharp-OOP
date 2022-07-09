929b0b7692a4c4f1ad55e88b3c8a8b8d405b775c
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-07-09 13:01:44 +0300

    Initial Commit
    Add Lab

---
+++ Interfaces and Abstraction/Lab/P02.Cars/Seat.cs
@@ -0,0 +1,29 @@
+﻿namespace Cars
+{
+    using System.Text;
+    public class Seat : ICar
+    {
+        public string Model { get; }
+        public string Color { get; }
+        public string Start() => "Engine start";
+
+        public string Stop() => "Breaaak!";
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+       
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} {nameof(Cars.Seat)} {Model}");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
