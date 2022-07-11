5c72f73d21f57bdab869760dd497d7b978e1967e
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-07-12 00:08:56 +0300

    Initial Commit
    Add Lab

---
+++ Polymorphism/Lab/P03.Shapes/Circle.cs
@@ -0,0 +1,16 @@
+﻿namespace Shapes
+{
+    using System;
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override double CalculatePerimeter() => 2 * Radius * Math.PI;
+        public override double CalculateArea() => Math.PI * Math.Pow(Radius, 2);
+        public override string Draw() => base.Draw() + this.GetType().Name;
+    }
+}
