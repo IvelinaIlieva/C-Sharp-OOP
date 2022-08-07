0a60bf01ca4b940eb5670c31d7f0fafb5a4a7105
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-08-07 04:01:56 +0300

    Initial Commit
    Add Exam

---
+++ !Exam/C# OOP Exam - 11 December 2021/Gym/Gym/IO/Writer.cs
@@ -0,0 +1,20 @@
+﻿namespace Gym.IO
+{
+    using Contracts;
+    using System;
+    using System.IO;
+
+    public class Writer : IWriter
+    {
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            //File.WriteAllText("output.txt", message);
+            Console.WriteLine(message);
+        }
+    }
+}
